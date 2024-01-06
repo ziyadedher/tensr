cc0480ce08f9af1423fb81524987a0669762481f
Ziyad Edher <ziyad.edher@gmail.com>
2024-01-06 08:50:59 -0800

    feat: introduce tensor3 permute

--- src/main.rs
+++ src/main.rs
@@ -8,4 +8,9 @@ fn main() {
 
     let e = Matrix::<f32, 3, 5>::ones();
     println!("{:?}", e.shape());
+
+    let a1 = Matrix::<f32, 3, 5>::zeros().permute::<1, 0>();
+
+    let b1 = Matrix::zeros();
+    let c1: Matrix<f32, 5, 3> = a1 + b1;
 }
--- src/tensor/tensor3.rs
+++ src/tensor/tensor3.rs
@@ -17,6 +17,82 @@ pub struct Tensor3<
     pub(crate) shape: <Tensor3<T, D0, D1, D2, B> as Tensor>::Shape,
 }
 
+impl<T, const D0: usize, const D1: usize, const D2: usize, B: Backend<T>>
+    Tensor3<T, D0, D1, D2, B>
+{
+    pub const fn construct_shape(d0: usize, d1: usize, d2: usize) -> (usize, usize, usize) {
+        (d0, d1, d2)
+    }
+
+    pub const fn calculate_permute(
+        (p0, p1, p2): (usize, usize, usize),
+        (d0, d1, d2): (usize, usize, usize),
+        i: usize,
+    ) -> usize {
+        match (p0, p1, p2, i) {
+            (0, 1, 2, 0) => d0,
+            (0, 1, 2, 1) => d1,
+            (0, 1, 2, 2) => d2,
+            (0, 2, 1, 0) => d0,
+            (0, 2, 1, 1) => d2,
+            (0, 2, 1, 2) => d1,
+            (1, 0, 2, 0) => d1,
+            (1, 0, 2, 1) => d0,
+            (1, 0, 2, 2) => d2,
+            (1, 2, 0, 0) => d2,
+            (1, 2, 0, 1) => d0,
+            (1, 2, 0, 2) => d1,
+            (2, 0, 1, 0) => d1,
+            (2, 0, 1, 1) => d2,
+            (2, 0, 1, 2) => d0,
+            (2, 1, 0, 0) => d2,
+            (2, 1, 0, 1) => d1,
+            (2, 1, 0, 2) => d0,
+            _ => panic!("improper permute"),
+        }
+    }
+
+    pub fn permute<const P0: usize, const P1: usize, const P2: usize>(
+        self,
+    ) -> Tensor3<
+        T,
+        {
+            Self::calculate_permute(
+                Self::construct_shape(P0, P1, P2),
+                Self::construct_shape(D0, D1, D2),
+                0,
+            )
+        },
+        {
+            Self::calculate_permute(
+                Self::construct_shape(P0, P1, P2),
+                Self::construct_shape(D0, D1, D2),
+                1,
+            )
+        },
+        {
+            Self::calculate_permute(
+                Self::construct_shape(P0, P1, P2),
+                Self::construct_shape(D0, D1, D2),
+                2,
+            )
+        },
+        B,
+    >
+    where
+        T: From<u8> + Copy,
+    {
+        Tensor3 {
+            repr: B::t3_permute(self.repr, (P0.into(), P1.into(), P2.into())),
+            shape: (
+                Self::calculate_permute((P0, P1, P2), (D0, D1, D2), 0),
+                Self::calculate_permute((P0, P1, P2), (D0, D1, D2), 1),
+                Self::calculate_permute((P0, P1, P2), (D0, D1, D2), 2),
+            ),
+        }
+    }
+}
+
 impl<T, const D0: usize, const D1: usize, const D2: usize, B: Backend<T>> Tensor
     for Tensor3<T, D0, D1, D2, B>
 {
